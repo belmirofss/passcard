08b443e33d6c7e609b2c66cee0bfcec828190d52
belmirofss <belmirofss@gmail.com>
2021-04-10 15:48:27 -0300

    correction to show interstitial

--- src/pages/Cards.tsx
+++ src/pages/Cards.tsx
@@ -73,7 +73,7 @@ export default function Cards() {
 
     React.useEffect(() => {
         ADMobService.showInterstitial();
-    });
+    }, []);
 
     React.useEffect(() => {
         setAlertVisible(route.params?.message ? true : false);
